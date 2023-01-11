e92dd3c136cd1b8e9bcf188b1adc86394ac78552
Angori4 <pavlichuk0810@gmail.com>
2023-01-11 03:12:35 +0200

    fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,77 +1,80 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-import Notiflix from 'notiflix';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const startBtn = document.querySelector('button[data-start]');
+const timer = document.querySelector('.timer');
+const dataDays = document.querySelector('span[data-days');
+const dataHours = document.querySelector('span[data-hours]')
+const dataMinutes = document.querySelector('span[data-minutes]')
+const dataSeconds = document.querySelector('span[data-seconds]')
+let timerId = null;
+startBtn.disabled = true;
 
 const input = document.querySelector('#datetime-picker');
-const btnStart = document.querySelector('button[data-start]');
-const timerHtml = document.querySelector('.timer');
-const timerSeconds = document.querySelector('span[data-seconds]');
-const timerMinutes = document.querySelector('span[data-minutes]');
-const timerHours = document.querySelector('span[data-hours]');
-const timerDays = document.querySelector('span[data-days]');
-
-let timer = null;
-
-btnStart.disabled = true;
-
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    if (selectedDates[0] < new Date()) {
-      Notiflix.Notify.failure('Please choose a date in the future');
-      btnStart.disabled = true;
-    } else {
-      btnStart.disabled = false;
-    }
-  },
+enableTime: true,
+time_24hr: true,
+defaultDate: new Date(),
+minuteIncrement: 1,
+    onClose(selectedDates) {
+    if (selectedDates[0] <= new Date) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+        startBtn.disabled = true;     
+    } else 
+        startBtn.disabled = false;
+    
+        console.log(selectedDates[0]);
+},
 };
 
 flatpickr(input, options);
 
-btnStart.addEventListener('click', onStartTimer);
-
-function onStartTimer() {
-  btnStart.disabled = true;
-
-  let timer = setInterval(() => {
-    const countDate = new Date(input.value) - new Date();
-    const { days, hours, minutes, seconds } = convertMs(countDate);
-
-    timerDays.textContent = addLeadingZero(days);
-    timerHours.textContent = addLeadingZero(hours);
-    timerMinutes.textContent = addLeadingZero(minutes);
-    timerSeconds.textContent = addLeadingZero(seconds);
-
-    console.log(`${days}:${hours}:${minutes}:${seconds}`);
-
-    if (countDate <= 1000) {
-      clearInterval(timer);
-    }
-  }, 1000);
-}
-
 function addLeadingZero(value) {
-  return value.toString().padStart(2, '0');
+    return String(value).padStart(2, '0');
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
+ 
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+startBtn.addEventListener('click', onClick);
+
+
+
+function onClick() {
+    timerId = setInterval(() => {
+
+    let countDown = new Date(input.value) - new Date();
+        if (countDown >= 0) {
+            let time = convertMs(countDown)
+   
+            dataDays.textContent = addLeadingZero(time.days);
+            dataHours.textContent = addLeadingZero(time.hours);
+            dataMinutes.textContent = addLeadingZero(time.minutes);
+            dataSeconds.textContent = addLeadingZero(time.seconds);
+            if (countDown <= 10000) {
+             timer.style.color = 'tomato';
+               }
+        } else {
+      Notiflix.Notify.success('CountDown finished');
+      timer.style.color = 'black';
+      clearInterval(timerId);
+     }
+       
+  }, 1000)
 }
