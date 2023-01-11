040859b4d109830e6f284fcd999d077d8d9da842
Angori4 <pavlichuk0810@gmail.com>
2023-01-11 02:52:09 +0200

    start

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,4 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from 'notiflix';
+
 
 const startBtn = document.querySelector('button[data-start]');
 const timer = document.querySelector('.timer');
